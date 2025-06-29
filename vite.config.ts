e254fa20e694818c08314d85f2e3515fb038b544
Rendyant Maulana <rendyanta.maulana@gmail.com>
2025-06-29 20:50:43 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig(async ({ mode }) => {
     },
     root: path.resolve(__dirname, "client"),
     build: {
-      outDir: path.resolve(__dirname, "dist"),
+      outDir: path.resolve(__dirname, "client", "dist"),
       emptyOutDir: true,
     },
     server: {
